74f4d9ea5e264d1ef155233926afe4e2d81bc277
Hoon H <drawtree@me.com>
2014-03-11 01:49:00 +0900

    Adjusted per-langauge header inclusion.

--- Application/Sources/Eonil/Improvisations/MediaEngine/Application/Application.h
+++ Application/Sources/Eonil/Improvisations/MediaEngine/Application/Application.h
@@ -9,13 +9,13 @@
 #ifndef __Application__Application__
 #define __Application__Application__
 
+
+#include	<Eonil/Improvisations/MediaEngine/Foundation/Foundation.h>
+
+#if	EE_LANG_CPP
 #include "Stepping.h"
 #include "Bundle.h"
-
 #include "run.h"
-
-#if	EE_LANG_OBJC
-//#include "Apple/iOS/PlatformStepping.h"
 #endif
 
 #endif /* defined(__Application__Application__) */
