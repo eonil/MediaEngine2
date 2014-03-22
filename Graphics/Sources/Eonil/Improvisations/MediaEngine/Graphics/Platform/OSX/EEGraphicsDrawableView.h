b9633bbcc3bd00343fb458420d321c58aad9ab98
Hoon H <drawtree@me.com>
2014-03-23 04:59:16 +0900

    Updated to use NSOpenGLContext directly.

--- Graphics/Sources/Eonil/Improvisations/MediaEngine/Graphics/Platform/OSX/EEGraphicsDrawableView.h
+++ Graphics/Sources/Eonil/Improvisations/MediaEngine/Graphics/Platform/OSX/EEGraphicsDrawableView.h
@@ -21,6 +21,43 @@
 #import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
 
+///*!
+// Provides a convenient view for OpenGL drawing.
+// 
+// @brief
+// OpenGL is multi-platform API, but obtaining process is all different by
+// platforms. This provides a ready-made drawable framebuffer/renderbuffer
+// for Cocoa touch in OS X.
+// 
+// @classdesign
+// Framebuffer and related renderbuffers will be created, and prepared.
+// They're immediately ready-to-use, and will be cleaned up when this object
+// dies.
+// 
+// @note
+// OS X version is a hurried implementation. It uses draw block instead of 
+// presentation of renderbuffer, and this is because I had no time to dig up
+// low level logic on OS X. This will be changed in future version.
+// 
+// Currently OS X version is mainly exists to preview purpose for iOS version.
+// */
+//@interface	EEGraphicsDrawableView : NSOpenGLView
+//- (id)		initWithFrame:(CGRect)frame;						//!	This is a designated initializer.
+//- (void)	prepareGraphicsContext;
+//- (void)	cleanupGraphicsContext;
+////- (void)	presentRenderbuffer;								//	Not supported yet.
+//@property	(readwrite,nonatomic,copy)	void(^_draw)(void);		//	Hurried implementation. Will be replaced with `presentRenderbuffer` method.
+//@end
+
+
+
+
+
+
+
+
+
+
 /*!
  Provides a convenient view for OpenGL drawing.
  
@@ -33,23 +70,13 @@
  Framebuffer and related renderbuffers will be created, and prepared.
  They're immediately ready-to-use, and will be cleaned up when this object
  dies.
- 
- @note
- OS X version is a hurried implementation. It uses draw block instead of 
- presentation of renderbuffer, and this is because I had no time to dig up
- low level logic on OS X. This will be changed in future version.
- 
- Currently OS X version is mainly exists to preview purpose for iOS version.
  */
-@interface	EEGraphicsDrawableView : NSOpenGLView
+@interface	EEGraphicsDrawableView2 : NSView
 - (id)		initWithFrame:(CGRect)frame;						//!	This is a designated initializer.
 - (void)	prepareGraphicsContext;
 - (void)	cleanupGraphicsContext;
-//- (void)	presentRenderbuffer;								//	Not supported yet.
-@property	(readwrite,nonatomic,copy)	void(^_draw)(void);		//	Hurried implementation. Will be replaced with `presentRenderbuffer` method.
+- (void)	presentRenderbuffer;
 @end
-
-
 
 
 
--- Graphics/Sources/Eonil/Improvisations/MediaEngine/Graphics/Platform/OSX/EEGraphicsDrawableView.mm
+++ Graphics/Sources/Eonil/Improvisations/MediaEngine/Graphics/Platform/OSX/EEGraphicsDrawableView.mm
@@ -39,69 +39,170 @@ using namespace	Eonil::Improvisations::MediaEngine::Graphics::Server;
 
 
 
-@implementation EEGraphicsDrawableView
-{
-}
+//@implementation EEGraphicsDrawableView
+//{
+//}
+//
+//- (id)initWithFrame:(NSRect)frameRect pixelFormat:(NSOpenGLPixelFormat *)format
+//{
+//	/*
+//	 This method shouldn't be deleted because this will be called by the superclass, and any exception will be silently
+//	 ignored.
+//	 */
+//	if (self = [super initWithFrame:frameRect pixelFormat:format])
+//	{
+//	}
+//	return	self;
+//}
+//- (id)initWithFrame:(CGRect)frame
+//{
+//	self	=	[super initWithFrame:frame];
+//	if (self)
+//	{
+//	}
+//	return	self;
+//}
+//- (id)init
+//{
+//	return	[self initWithFrame:CGRectZero];
+//}
+//- (void)dealloc
+//{
+//}
+//
+//- (void)prepareGraphicsContext
+//{
+//	
+//}
+//- (void)cleanupGraphicsContext
+//{
+//
+//}
+//- (void)presentRenderbuffer
+//{
+//
+//}
+//
+//
+//
+//- (void)drawRect:(NSRect)dirtyRect
+//{
+//	EONIL_DEBUG_ASSERT([[NSThread currentThread] isEqual:[NSThread mainThread]]);
+//	EONIL_DEBUG_ASSERT(self.superview.wantsLayer);
+//	EONIL_DEBUG_ASSERT(self.superview.layer != nil);
+//	
+//	////
+//	
+//	[super drawRect:dirtyRect];
+//	
+//	////
+//	
+//	{
+//		[self.openGLContext makeCurrentContext];
+//		Renderbuffer	rb1		{Renderbuffer::FORMAT::DEPTH16, self.frame.size.width, self.frame.size.height};
+//		eeglFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rb1.name());
+//		self._draw();
+//		[self.openGLContext flushBuffer];
+//		eeglFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, 0);
+//	}
+//}
+//@end
 
-- (id)initWithFrame:(NSRect)frameRect pixelFormat:(NSOpenGLPixelFormat *)format
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@implementation EEGraphicsDrawableView2 
 {
-	/*
-	 This method shouldn't be deleted because this will be called by the superclass, and any exception will be silently
-	 ignored.
-	 */
-	return	[super initWithFrame:frameRect pixelFormat:format];
+	NSOpenGLContext*	_gl_ctx;
 }
 - (id)initWithFrame:(CGRect)frame
 {
 	self	=	[super initWithFrame:frame];
 	if (self)
 	{
+		NSOpenGLPixelFormatAttribute	SENTINEL		=	0;
+		NSOpenGLPixelFormatAttribute	pixelAttrs[]	=
+		{
+			NSOpenGLPFADoubleBuffer,
+//			NSOpenGLPFAOpenGLProfile,	NSOpenGLProfileVersion3_2Core,
+			/*!
+			 It is very important that OpenGL 3.2+ is not compatible with
+			 OpenGL ES 2.0, and the behavioral differences will lead program to crash.
+			 So always use *legacy* profile. 
+			 
+			 GLSL ES 1.00 is very close to GLSL (DT) 1.2. GLSL (DT) 1.1 and 1.2 is almost
+			 compatible, just revision update. GLSL (DT) 1.3+ is a new langauge, so it's
+			 not compatible.
+			 
+			 @ref
+			 http://stackoverflow.com/questions/10345323/is-opengl-backwards-compatible-with-opengl-es
+			 */
+			NSOpenGLPFAOpenGLProfile,	NSOpenGLProfileVersionLegacy,
+			NSOpenGLPFAColorSize,		24,
+			NSOpenGLPFAAlphaSize,		8,
+			NSOpenGLPFADepthSize,		24,
+			NSOpenGLPFAStencilSize,		8,
+			NSOpenGLPFASampleBuffers,	1,
+			NSOpenGLPFAMultisample,		
+			NSOpenGLPFASamples,			4,
+			SENTINEL,
+		};
+		
+		NSOpenGLPixelFormat*			pf1				=	[[NSOpenGLPixelFormat alloc] initWithAttributes:pixelAttrs];
+		
+		_gl_ctx	=	[[NSOpenGLContext alloc] initWithFormat:pf1 shareContext:nil];
 	}
 	return	self;
 }
-- (id)init
-{
-	return	[self initWithFrame:CGRectZero];
-}
-- (void)dealloc
-{
-}
-
 - (void)prepareGraphicsContext
 {
+	EONIL_DEBUG_ASSERT_WITH_MESSAGE([self.window isKindOfClass:[NSWindow class]], "This method must be called while this view is placed on a visible `UIWindow` to configure OpenGL rendering surface properly.");
+	EONIL_DEBUG_ASSERT(_gl_ctx != nil);
 	
+	[_gl_ctx setView:self];
+	[_gl_ctx makeCurrentContext];
 }
 - (void)cleanupGraphicsContext
 {
-
+	EONIL_DEBUG_ASSERT_WITH_MESSAGE([self.window isKindOfClass:[NSWindow class]], "This method must be called while this view is placed on a visible `UIWindow` to be matched with `prepareGraphicsContext` method.");
+	EONIL_DEBUG_ASSERT(_gl_ctx != nil);
+	
+	[_gl_ctx setView:nil];
+	[NSOpenGLContext clearCurrentContext];
 }
 - (void)presentRenderbuffer
 {
-
+	EONIL_DEBUG_ASSERT([_gl_ctx view] == self);
+	EONIL_DEBUG_ASSERT([_gl_ctx isEqual:[NSOpenGLContext currentContext]]);
+	
+	[_gl_ctx flushBuffer];
 }
-
-
-
-- (void)drawRect:(NSRect)dirtyRect
+- (void)setFrame:(NSRect)frameRect
 {
-	EONIL_DEBUG_ASSERT(self.superview.wantsLayer);
-	EONIL_DEBUG_ASSERT(self.superview.layer != nil);
-	
-	////
-	
-	[super drawRect:dirtyRect];
-	
-	////
-	
-	{
-		Renderbuffer	rb1		{Renderbuffer::FORMAT::DEPTH16, self.frame.size.width, self.frame.size.height};
-		eeglFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rb1.name());
-		self._draw();
-		[self.openGLContext flushBuffer];
-		eeglFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, 0);
-	}
+	[super setFrame:frameRect];
+	[_gl_ctx update];
 }
 @end
+
+
+
+
+
+
+
+
 
 
 
