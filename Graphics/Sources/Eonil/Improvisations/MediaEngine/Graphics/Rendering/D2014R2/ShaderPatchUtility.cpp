542372b9bc3312cb0c86c3580f37d2256e8230a1
Hoon H <drawtree@me.com>
2014-03-26 04:42:01 +0900

    Removed ATI driver bug fix. The bug was not exist, and it was my fault on GPU memory alignment.

--- Graphics/Sources/Eonil/Improvisations/MediaEngine/Graphics/Rendering/D2014R2/ShaderPatchUtility.cpp
+++ Graphics/Sources/Eonil/Improvisations/MediaEngine/Graphics/Rendering/D2014R2/ShaderPatchUtility.cpp
@@ -41,20 +41,20 @@ namespace Eonil { namespace Improvisations { namespace MediaEngine { namespace G
 #	include		"Common.fragment.shader.glsl-dt-1_10"
 				;
 				
-				if (Stub::eeglGetVendorIsATI())
-				{
-					/*!
-					 ATI driver bug. It reports `gl_MaxVertexUniformComponents` as quad multiplied value.
-					 It must be divided by 4 to be the correct value.
-					 
-					 @todo
-					 Check again this whether this bug is really exists. This may because of the
-					 GLSL float-aligning behavior rather than an actual bug.
-					 */
-					vs_prefix_macros	+=
-#	include			"Common.vertex.shader.glsl-dt-1_10-ATI-driver-bug-fix"
-					;	
-				}
+//				if (Stub::eeglGetVendorIsATI())
+//				{
+//					/*!
+//					 ATI driver bug. It reports `gl_MaxVertexUniformComponents` as quad multiplied value.
+//					 It must be divided by 4 to be the correct value.
+//					 
+//					 @todo
+//					 Check again this whether this bug is really exists. This may because of the
+//					 GLSL float-aligning behavior rather than an actual bug.
+//					 */
+//					vs_prefix_macros	+=
+//#	include			"Common.vertex.shader.glsl-dt-1_10-ATI-driver-bug-fix"
+//					;	
+//				}
 				
 //				vs_prefix_macros	+=
 //				R"(
